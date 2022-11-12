9c3ea4b3446634ea72ce8ee3c65ef3f63007ed8b
Osmar Menezes da Silva <adm.osmarsilva@gmail.com>
2022-11-12 08:45:16 -0300

    refinado

--- projeto/js/scripts.js
+++ projeto/js/scripts.js
@@ -1,18 +1,13 @@
-//select elements
 const menuBtn = document.querySelector('#menu');
 const closeMenuBtn = document.querySelector('#close-menu');
 const menu = document.querySelector('#mobile-navbar');
-
 const desktopLinks = document.querySelectorAll('#navbar a');
 const mobileLinks = document.querySelectorAll('#mobile-navbar a');
 const allLinks = [...desktopLinks, ...mobileLinks];
-
 const slides = document.querySelectorAll('.banner');
 const dots = document.querySelectorAll('.dot');
 let slideIndex = 0;
 
-
-//functions
 function smoothScroll(e) {
     e.preventDefault();
     const href = this.getAttribute('href');
@@ -21,7 +16,6 @@ function smoothScroll(e) {
         top: offsetTop,
         behavior: "smooth"
     });
-
     setTimeout(() => {
         if(menu.classList.contains('menu-active')) {
             menu.classList.remove('menu-active');
@@ -34,21 +28,15 @@ function showSlides() {
         slides[i].classList.remove('active');
         dots[i].classList.remove('active');
     }
-
     slideIndex++;
-
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-
     slides[slideIndex - 1].classList.add('active');
     dots[slideIndex - 1].classList.add('active');
-
     setTimeout(showSlides, 3000)
-
 }
 
-//events
 [menuBtn, closeMenuBtn].forEach((btn) => {
     btn.addEventListener('click', (e) => {
         menu.classList.toggle('menu-active');
